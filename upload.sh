d2d218491cea61331eb61b054a0663fccdb25633
Isaac Imholt <isaacimholt@users.noreply.github.com>
2020-05-16 20:13:43 +0200

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -10,7 +10,6 @@ eval $(parse_yaml config.yaml)
 verify_param uploader_youtube_source
 
 
-cd $uploader_youtube_source
-for f in *; do
+for f in find $uploader_youtube_source -maxdepth 1 -type f; do
     echo "Testing $f file..."
 done
