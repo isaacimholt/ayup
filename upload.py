205a38da6e8f4225a344a964b1be0f9621d9e622
Isaac Imholt <isaacimholt@users.noreply.github.com>
2021-01-17 21:54:36 +0100

    Revert "removed module format str since it was not working"
    This reverts commit 7b55c8140ba0729fec4d9dd22589fe2069d21162.

--- upload.py
+++ upload.py
@@ -8,7 +8,7 @@ import yaml
 logging.basicConfig(
     filename="upload.log",
     level=logging.INFO,
-    format="%(asctime)s.%(msecs)03d %(levelname)s - %(funcName)s: %(message)s",
+    format="%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
